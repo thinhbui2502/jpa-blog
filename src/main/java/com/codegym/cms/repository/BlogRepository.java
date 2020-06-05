e2e3cbca3a58e955dfb829590f0159b83c555375
thinhbui2502 <buiducthinh2502@gmail.com>
2020-06-05 09:53:32 +0700

    add file jpa-blog

---
+++ src/main/java/com/codegym/cms/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.codegym.cms.repository;
+
+import com.codegym.cms.model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Long> {
+}
---
+++ src/main/java/com/codegym/cms/service/impl/BlogServiceImpl.java
@@ -0,0 +1,34 @@
+package com.codegym.cms.service.impl;
+
+import com.codegym.cms.model.Blog;
+import com.codegym.cms.repository.BlogRepository;
+import com.codegym.cms.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BlogServiceImpl implements BlogService {
+    @Autowired
+    private
+    BlogRepository blogRepository;
+
+    @Override
+    public List<Blog> findAll() {
+        return blogRepository.findAll();
+    }
+
+    @Override
+    public Blog findById(Long id) {
+        return blogRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Blog blog) {
+        blogRepository.save(blog);
+    }
+
+    @Override
+    public void remove(Long id) {
+        blogRepository.delete(id);
+    }
+}
